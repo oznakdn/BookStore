afaf1a78bda1f898523d26a53e80852e14165d13
ozan lütfü akaydın <ozanakaydin@gmail.com>
2021-12-13 21:08:18 +0300

    Patika.dev .Net Core WebApi BookStore

---
+++ BookStore.WebApi/DBOperations/BookStoreDbContext.cs
@@ -0,0 +1,18 @@
+using BookStore.WebApi.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStore.WebApi.DBOperations
+{
+    public class BookStoreDbContext:DbContext
+    {
+        public BookStoreDbContext(DbContextOptions<BookStoreDbContext>options):base(options)
+        {
+            
+        }
+
+        public DbSet<Book>Books{get;set;}
+        public DbSet<Genre>Genres{get;set;}
+        public DbSet<Author>Authors{get;set;}
+        
+    }
+}
